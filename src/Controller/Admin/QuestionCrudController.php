1a190d3c1a9618df986fe891371eba25cf35b3d4
luangouveiax <luan.gouveiax@gmail.com>
2025-01-22 00:46:14 -0300

    AssociationField

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -4,7 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\Question;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -15,14 +18,18 @@ class QuestionCrudController extends AbstractCrudController
         return Question::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')
+            ->onlyOnIndex();
+        yield AssociationField::new('topic');
+        yield Field::new('name');
+        yield TextareaField::new('question')
+            ->hideOnIndex();
+        yield Field::new('votes', 'Total Votes')
+            ->setTextAlign('right');
+        yield AssociationField::new('askedBy');
+        yield Field::new('createdAt')
+            ->hideOnForm();
     }
-    */
 }
